1ef7bdb3ce8f5e4e316df884c892a0d6cd2f4067
Raj Kumar Roul <55204735+rajk12-ux@users.noreply.github.com>
2025-02-17 00:02:22 +0530

    [219] [Time Beats: 21.01%] [Memory Beats: 5.16%] - LeetPush

--- 219-Contains-Duplicate-II.cpp
+++ 219-Contains-Duplicate-II.cpp
@@ -4,15 +4,14 @@ public:
         unordered_map<int,set<int>> indcs;
         for(int i=0;i<nums.size();i++){
             indcs[nums[i]].insert(i);
-        }
-        for(pair<int,set<int>> my_indcs : indcs){
-            auto it=my_indcs.second.begin();
-            auto s =++my_indcs.second.begin();
-            while(s!= my_indcs.second.end()){
-                if(abs(*s - *it)<=k)return true;
-                ++it;++s;
+            if(indcs[nums[i]].size()==2){
+                if(*indcs[nums[i]].rbegin()-*indcs[nums[i]].begin() <= k){
+                    return true;
+                }
+                else indcs[nums[i]].erase(indcs[nums[i]].begin());
             }
         }
         return false;
+        
     }
 };
