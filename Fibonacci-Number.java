14df8d69c5e7a64d12772eb8db0416bf9a6e04ec
Raj Kumar Roul <55204735+rajk12-ux@users.noreply.github.com>
2025-03-18 01:20:12 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 79.87%] - LeetPush

---
+++ Fibonacci-Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        if(n<2)return n;
+        int curr=1,prev=1,pprev=0;
+        n-=2;
+        while(n-- >0){
+            pprev=prev;
+            prev=curr;
+            curr += pprev;
+        }
+        return curr;
+    }
+}
