b9353d75d17d60632384a9d8e6207d1e3a5b43e5
Raj Kumar Roul <55204735+rajk12-ux@users.noreply.github.com>
2025-03-23 00:17:13 +0530

    [3115] [Time Beats: 54.51%] [Memory Beats: 96.88%] - LeetPush

--- Maximum-Prime-Difference.java
+++ Maximum-Prime-Difference.java
@@ -1,28 +1,15 @@
 class Solution {
     public int maximumPrimeDifference(int[] nums) {
         Set<Integer> primes = new HashSet<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97));
-        int first = -1, second = -1;
-        int i = 0;
-
-        // Find the first index of a prime number
-        for (; i < nums.length; i++) {
-            if (primes.contains(nums[i])) {
-                first = i;
-                break;
-            }
+        int l=0,r=nums.length-1;
+        while(l<nums.length){
+            if(primes.contains(nums[l]))break;
+            ++l;
         }
-        // Continue searching for the last index of a prime number
-        i++;
-        for (; i < nums.length; i++) {
-            if (primes.contains(nums[i])) {
-                second = i;
-            }
+        while(r>0){
+            if(primes.contains(nums[r]))break;
+            --r;
         }
-        // If no second prime number is found, return 0
-        if (second == -1) {
-            return 0;
-        }
-        // Return the difference between the indices
-        return second - first;
+        return r-l;
     }
 }
