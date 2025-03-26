39def1e958510ac28ea078dd3bd3f960e9646d6e
Raj Kumar Roul <55204735+rajk12-ux@users.noreply.github.com>
2025-03-26 21:49:48 +0530

    [3146] [Time Beats: 100.00%] [Memory Beats: 86.68%] - LeetPush

--- Permutation-Difference-between-Two-Strings.java
+++ Permutation-Difference-between-Two-Strings.java
@@ -1,13 +1,13 @@
 class Solution {
     public int findPermutationDifference(String s, String t) {
-        Map<Character, Integer> p = new HashMap<>();
+        int[] p = new int[26]; 
         for (int i = 0; i < s.length(); i++) {
-            p.put(s.charAt(i), i);
+            p[s.charAt(i) - 'a'] = i;
         }
-        int sumd = 0;
+        int sumd = 0; 
         for (int i = 0; i < t.length(); i++) {
-            sumd += Math.abs(p.get(t.charAt(i)) - i);
+            sumd += Math.abs(p[t.charAt(i) - 'a'] - i); // Calculate and sum differences
         }
-        return sumd;
+        return sumd; // Return the total sum of differences
     }
 }
